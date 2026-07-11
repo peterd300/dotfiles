67b7a6c106720861de784caab8f5710e3676968d
Peter <peterd300@gmail.com>
2026-07-11 15:31:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,34 +17,28 @@ sudo ln -s /etc/sv/vmtoolsd /var/service/
 # first install some usefull programs
 sudo xbps-install -Sy htop btop make git wget unzip nano cmake curl gcc net-tools fastfetch
 
-
 # install xorg
 sudo xbps-install -Syu
 sudo xbps-install -Sy xorg xorg-server xorg-apps xrandr xterm xscreensaver twm
 
-
 # install openbox
 sudo xbps-install -Sy openbox obconf obmenu-generator obconf-qt lxappearance lxappearance-obconf
-
 mkdir -p ~/.config/openbox
 sleep 1
 cp /etc/xdg/openbox/rc.xml ~/.config/openbox/
-
 cp /etc/xdg/openbox/autostart ~/.config/openbox/
 cp /etc/xdg/openbox/menu.xml ~/.config/openbox/
 obmenu-generator -p -i
 sleep 2
 
+
 echo "exec xterm & " >> ~/.xinitrc
 echo "exec openbox-session" >> ~/.xinitrc
 echo "# exec twm" >> ~/.xinitrc
 
-
 ## install openbox utils
-sudo xbps-install -Sy polybar tint2 feh xdg-user-dirs xdg-utils nitrogen 
+sudo xbps-install -Sy polybar tint2 dunst rofi feh xdg-user-dirs xdg-utils nitrogen 
 mkdir -p ~/.config/polybar
-
-
 
 ## install elogind
 sudo xbps-install -Sy elogind dbus-elogind 
@@ -58,7 +52,6 @@ sudo ln -s /etc/sv/elogind /var/service/
 ## install dbus
 sudo xbps-install -Sy dbus
 sudo ln -s /etc/sv/dbus /var/service/
-
 
 ## install x11 utils (icon theme switch werkt nog niet)
 sudo xbps-install -Sy adwaita-plus turnstile nerd-fonts font-awesome 
@@ -74,7 +67,6 @@ sudo xbps-install -Sy Thunar thunar-archive-plugin thunar-media-tags-plugin tumb
 #install X11 icon themes
 sudo xbps-install -Sy papirus-icon-theme lxde-icon-theme xcursor-themes
 
-
 #install geany
 sudo xbps-install -Sy geany geany-editorconfig-plugin geany-plugins geany-plugins-extra
 
@@ -88,15 +80,11 @@ sleep 2
 mkdir -p ~/.config/picom
 cp /usr/share/examples/picom/picom.sample.conf ~/.config/picom/picom.conf
 
-
 ## install sound support
 sudo xbps-install -Sy pipewire alsa-plugins-pulseaudio wireplumber pavucontrol pamixer
 
-
-## give user access to audio and video device
-
+## give local user access to audio and video device
 sudo usermod -aG audio,video $(whoami)
-
 
 # create tmp dir for pipewire, wthout pipwire  don't start
 #.bashrc
@@ -110,7 +98,7 @@ obmenu-generator -p -i
 # ~/.config/openbox/autostart
 
 echo "sleep 1 && pipewire &" >> ~/.config/openbox/autostart
-echo " sleep 1 && wireplumber &" >> ~/.config/openbox/autostart
+echo "sleep 1 && wireplumber &" >> ~/.config/openbox/autostart
 echo "sleep 1 && pipewire-pulse &" >> ~/.config/openbox/autostart
 echo "sleep 1 " >> ~/.config/openbox/autostart
 echo "sleep 1 && polybar &" >> ~/.config/openbox/autostart
