fa338f8744e6fdcb91a7346b80e6bf945cb8d75f
Peter <peterd300@gmail.com>
2026-07-27 20:26:35 +0200

    pywal

---
+++ pywal/install_pywal.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# installation of pywall via the repo
+# install walbox - pywal theme for openbox
+# copy several config files for implementing for pywall theme
+
+cd ~/dotfiles/pywall
+
+sudo xbps-install -Suy
+sudo xbps-install -Sy gtk2-engines pywal
+
+# install walbox theme for openbox
+git clone https://github.com/edisile/walbox.git
+cd walbox
+./install.sh
+cd ..
+
+cp -R ~/dotfiles/pywall/.config/* ~/.config/.
+
+
+openbox --reconfigure
