fc215370d3f50877b4cae9797753fc8fed4cc5ce
Peter <peterd300@gmail.com>
2026-07-29 12:59:21 +0200

    pywal 16

---
+++ pywal16/install16.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# install pywal16 via pip3
+sudo xbps-install -S python3-pipenv python3-pipx ImageMagick
+
+pipx install pywal16
+
+pipx ensurepath
+echo " eval "$(register-python-argcomplete pipx)" " >> ~/.bashrc
+
+source ~/.bashrc
+
+
+
+# install walbox theme for openbox
+git clone https://github.com/edisile/walbox.git
+cd walbox
+./install.sh
+cd ..
+
+
+rm -rf .git
+
+
+# copy several config files
+cp -Rv ~/dotfiles/pywal16/.config/* ~/.config
+which wal
+wal -v
+
+
