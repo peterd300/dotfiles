238adbb7cf7c75652e75d7eae645bf8fb0f19cd1
Peter <peterd300@gmail.com>
2026-07-29 15:30:42 +0200

    chwal.sh

--- pywal16/chwal.sh
+++ pywal16/chwal.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 wal -i $1 -n && feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
 openbox --reconfigure
+
+
+# Custom 16-color wallpaper changer for Openbox, add this function to .bashrc
+#
+# wal16() {
+#     wal -n -i "$1" && feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
+# }
+# example : wal16 wallpaper.jpg
