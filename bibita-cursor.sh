0d597c568390b7a99bead5eb455a23a6e9419e52
Peter <peterd300@gmail.com>
2026-08-10 15:58:47 +0200

    bibita.sh

--- bibita-cursor.sh
+++ bibita-cursor.sh
@@ -7,12 +7,18 @@ mkdir -p ~/.local/share/icons
 wget https://github.com/ful1e5/Bibata_Cursor/releases/download/$latest_version/Bibata-Modern-Classic.tar.xz
 wget https://github.com/ful1e5/Bibata_Cursor/releases/download/$latest_version/Bibata-Modern-Ice-Right.tar.xz
 
-
+# extract file with sleep , otherwise errors
 tar -xvf Bibata-Modern-Classic.tar.xz
 sleep 2                # extract `Bibata.tar.gz`
 tar -xvf Bibata-Modern-Ice-Right.tar.xz
 sleep 2
+
+
+# move files to local share
 mv Bibata-Modern-Classic/ ~/.local/share/icons/
 mv Bibata-Modern-Ice-Right/ ~/.local/share/icons/   # Install to local users
 
-rm -rf Bibata-Modern-*.tar.xz
+echo "Xcursor.theme: Bibata-Modern-Classic" >> ~.Xresources
+
+
+rm -rf ./Bibata-Modern-*.tar.xz
--- pywal16/chwal.sh
+++ pywal16/chwal.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#/usr/bin/env bash
 wal -i $1 -n && feh --bg-scale "$(< "${HOME}/.cache/wal/wal")"
 openbox --reconfigure
 
